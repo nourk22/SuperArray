445f874f2cec56b570c76c68854cacb90fab7e0c
Nour <ckastoun20@stuy.edu>
2020-11-13 00:46:33 -0500

    added throwing exceptions, tested, it works, tests are commented out

--- SuperArray.java
+++ SuperArray.java
@@ -14,6 +14,9 @@ public class SuperArray{
 
 // 15. constructor
   public SuperArray(int initialCapacity){ // create superarray with given starting capacity
+    if (initialCapacity<0){
+      throw new IllegalArgumentException ("Initial capacity " + initialCapacity + " cannot be negative, you pleb");
+    }
     data = new String[initialCapacity];
     size = 0;
     capacity = initialCapacity;
@@ -42,11 +45,17 @@ public class SuperArray{
 
 // 14. (e) get method: get element at index
   public String get(int index){
+    if (index<0 || index >= size()){
+      throw new IndexOutOfBoundsException ("Index " + index + " cannot be negative or greater than the current size of the SuperArray. Smh.");
+    }
     return data[index];
   }
 
 // 14. (f) set method: change element at index
   public String set(int index, String element){
+    if (index<0 || index >= size()){
+      throw new IndexOutOfBoundsException ("Index " + index + " cannot be out of range. Come on dude. I'm disappointed.");
+    }
     String old_val=data[index];
     data[index]=element;
     return old_val;
@@ -105,6 +114,9 @@ public class SuperArray{
 
 // 15. inserts element, shifts right if needed
   public void add(int index, String element){
+    if (index<0 || index > size()){
+      throw new IndexOutOfBoundsException ("Index " + index + " cannot be out of range. Sigh.");
+    }
     if (size>=capacity){
       resize();
     }
@@ -117,6 +129,9 @@ public class SuperArray{
 
 // 15. removes element, shifts left
   public String remove(int index){
+    if (index<0 || index >= size()){
+      throw new IndexOutOfBoundsException ("Index " + index + " cannot be negative or greater than the current size of the SuperArray. *facepalm*");
+    }
     String elementRemoved=data[index];
     for (int i=index; i<capacity-1; i++){
         data[i]=data[i+1]; //shift left (no need for removal, shifting removes it automatically)
@@ -180,5 +195,49 @@ public class SuperArray{
 
 
   public static void main(String[] args){
+/*
+    SuperArray test = new SuperArray(10);
+    SuperArray test2 = new SuperArray(0);
+  //  SuperArray testError = new SuperArray(-10);
+
+    test.add("hi");              test.add("hello");            test.add("howdy");
+    test.add("greetings");       test.add("good morning");     test.add("hihi");
+    test.add("bonjour");         test.add("salut");
+    test.add("buenos días");     test.add("hola");
+
+    test.get(1); //should work
+  //  test.get(10); //should throw error
+  //  test.get(200); //should throw error
+    test.get(0); //should work
+  //  test.get(-10); //should throw error
+
+    test.set(1, "pie"); //should work
+  //  test.set(-1, "piepie"); //should not work
+    test.set(0, "piepiepie"); //should work
+  //  test.set(10, "piepiepiepie"); //should not work
+  //  test.set(200, "piepiepiepiepie"); //should not work
+
+    test.add(10, "PIE"); //should work
+  //  test.add(12, "PIE!!"); //should not work
+  //  test.add(-1, "PIE PIE PIE PIE PIE PIE"); //should not work
+
+  //  test.remove(11); //should not work
+  //  test.remove(-1); //should not work
+
+    try {
+    // Code bock that can potentially throw an exception.
+    //  If they are not caught, they are said to propagate to the next method.
+    SuperArray testError = new SuperArray(-10);
+    System.out.println("success");
+    }
+    catch (IllegalArgumentException e1) {
+        // Handle ExceptionType1 exceptions
+        System.out.println("fail");
+    }
+    catch (RuntimeException e2) {
+        // Handle ExceptionType2 exceptions
+        System.out.println("even more fail");
+    }
+    */
   }
 }
